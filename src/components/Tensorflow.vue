5b6a79b4c63541b3d6c5b09d6882fb02e966383e
kalam ahmed <kalamahmed714@gmail.com>
2021-01-24 01:16:59 +0600

    object detection with coco-ssd

--- src/components/Tensorflow.vue
+++ src/components/Tensorflow.vue
@@ -3,6 +3,32 @@
      <div class="m-auto">
          <div class="mt-10">
          <h1 class="text-2xl my-4">Object Detection</h1>
+         <div class="flex flex-wrap justify-center my-2">
+             <div class="w-full flex justify-center ">
+                    <button
+                    v-if="!isStreaming"
+                  @click="openCamera"
+              class="w-32 rounded shadow-md bg-gradient-to-r from-red-900 to-blue-800 text-white p-2"
+             >Open Camera</button>
+             <div class="flex justify-between" v-else> 
+                    <button
+               
+                  @click="stopStreaming"
+              class="w-32 rounded shadow-md bg-gradient-to-r from-red-900 to-blue-800 text-white p-2"
+             >Stop Streaming</button>
+                  <button
+               
+                  @click="snapShot"
+              class="w-32 rounded shadow-md bg-gradient-to-r from-red-900 to-gray-800 text-white p-2"
+             >Snap</button>
+
+             </div>
+
+           
+          
+             </div>
+          <video autoplay="true" width="200" ref="videoRef" />
+         </div>
          <div class="flex flex-wrap justify-center">
                 <img 
                 ref='imgRef'
@@ -38,6 +64,8 @@ export default {
     setup(){
         const imgRef =ref('')
         const  isLoading =ref(false)
+        const isStreaming=ref(false)
+        const videoRef= ref('')
         const result =ref('')
 
    async function detect(){
@@ -50,8 +78,35 @@ export default {
       isLoading.value=false
 
    }
+  async function openCamera(){
+       if(navigator.mediaDevices.getUserMedia){
+           const devices =await navigator.mediaDevices.enumerateDevices()
+           const cams =(devices.filter((devices) => devices.kind ==='videoinput' ))
+            const camId =cams[0].deviceId
+           navigator.mediaDevices.getUserMedia({video:{deviceId:{exact:camId}}}).then((stream)=>{
+               isStreaming.value=true
+               videoRef.value.srcObject = stream
+           })
+       }
+   }
+   function stopStreaming(){
+       const stream = videoRef.value.srcObject
+       const tracks = stream.getTracks()
+       tracks.map((track)=>track.stop())
+       isStreaming.value=false
 
-        return {imgRef,result,detect,isLoading}
+   }
+   function snapShot(){
+       const canvas = document.createElement('canvas')
+       const ctx = canvas.getContext('2d')
+       ctx.drawImage(videoRef.value,0,0,200,200)
+       const data = canvas.toDataURL('image/png')
+       imgRef.value.setAttribute('src',data)
+      
+
+   }
+
+        return {imgRef,result,detect,isLoading,videoRef,openCamera,isStreaming,stopStreaming,snapShot}
     }
 
 }
